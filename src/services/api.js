75098eae8c56dd35c63c662a9b6e88863934e6e5
Tales Rodrigues <tales00001@gmail.com>
2022-07-07 16:12:23 -0300

    Resolução dos Requisitos 1 e 2 - Correção do Lint

--- src/services/api.js
+++ src/services/api.js
@@ -1,11 +1,9 @@
 export async function getCategories() {
-  // Implemente aqui
   const response = await fetch('https://api.mercadolibre.com/sites/MLB/categories');
   return response.json();
 }
 
 export async function getProductsFromCategoryAndQuery(categoryId, query) {
-  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
   let response = '';
   if (!categoryId && query) {
     response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${query}`);
